dddbe07237e5915cb439f5d1d6528d6a6b960be9
wjchen0623 <chenweijun88@gmail.com>
2023-11-16 07:47:40 +0000

    test original

--- Hello.py
+++ Hello.py
@@ -1,8 +1,14 @@
 import streamlit as st
 from streamlit.logger import get_logger
 import json
+import re
 
 LOGGER = get_logger(__name__)
+
+@st.cache_data
+def read_json_into_dict(json_path):
+   json_dict = json.load(json_path)
+   return (json_dict)
 
 def run():
     st.set_page_config(
@@ -10,27 +16,37 @@ def run():
         page_icon="👩‍⚕️",
     )
 
-    st.write("# Welcome to Bot MD Care Vector Search")
-    
+    st.write("## Welcome to Bot MD Care Vector Search")
+
     st.markdown(
         """
         Demo to showcase what vector search can do for Bot MD Care.
     """
     )
 
+    query = st.text_input('Chat', 'What time are you open?')
+
+    if query is None:
+       st.warning("Please enter a query")
+
     col1, col2, col3 = st.columns(3)
 
     with col1:
       st.header("Original")
-      st.image("https://static.streamlit.io/examples/cat.jpg")
+      if query is not None:
+         original_dict = read_json_into_dict("data/ALTY/faq/faq_dict_old.json")
+         old_query = re.sub(r"\s+", "", query)
+         st.write(original_dict[old_query])
 
     with col2:
-      st.header("Enhanced")
-      st.image("https://static.streamlit.io/examples/dog.jpg")
+      st.header("Vector Search")
+      if query is not None:
+         st.image("https://static.streamlit.io/examples/cat.jpg")
 
     with col3:
-      st.header("Generative AI")
-      st.image("https://static.streamlit.io/examples/owl.jpg")
+      st.header("Vector Search + Generative AI")
+      if query is not None:
+         st.image("https://static.streamlit.io/examples/cat.jpg")
 
 
 if __name__ == "__main__":
